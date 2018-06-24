219132efbe48259d6e4facff0cc06669a0d32bff
smensito <alberto.nii@hotmail.com>
2018-06-24 15:28:42 +0200

    Ahora al crear un experimento ya no se va a la url /start (el problema era que si se refrescaba la pg se ejecutaba de nuevo el programa)
    Formularios de profile validados. Hay error al validar formulario de aboutMe

--- src/main/java/com/gramevapp/web/model/UserUpdateAboutDto.java
+++ src/main/java/com/gramevapp/web/model/UserUpdateAboutDto.java
@@ -1,23 +1,13 @@
 package com.gramevapp.web.model;
 
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Pattern;
 
 public class UserUpdateAboutDto {
 
-    private static final String PATTERN_NUM_LETTERS = "^[\\p{L} \\d .'-]+$";
-
-    private Long id;
+    private static final String PATTERN_NUM_LETTERS = "^[\\p{L} \\d .'-]*$";
 
     @Pattern(regexp = PATTERN_NUM_LETTERS, message = "Your user information cannot contain strange characters")
     private String aboutMe;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getAboutMe() {
         return aboutMe;
--- src/main/java/com/gramevapp/web/model/UserUpdateStudyDto.java
+++ src/main/java/com/gramevapp/web/model/UserUpdateStudyDto.java
@@ -4,22 +4,12 @@ import javax.validation.constraints.Pattern;
 
 public class UserUpdateStudyDto {
 
-    private static final String PATTERN_NUM_LETTERS = "^[\\p{L} \\d .'-]+$";
+    private static final String PATTERN_NUM_LETTERS = "^[\\p{L} \\d .'-]*$";
 
-    private Long id;
-
-    // This must best a list<String> listStudy and listWork, but for now... This is ok
     @Pattern(regexp = PATTERN_NUM_LETTERS, message = "The study information cannot contain strange characters")
     private String studyInformation;
     @Pattern(regexp = PATTERN_NUM_LETTERS, message = "The work information cannot contain strange characters")
     private String workInformation;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getStudyInformation() {
         return studyInformation;
