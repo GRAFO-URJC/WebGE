477b7549a2a4642e562f931099084873c9682f4f
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2019-07-13 15:48:17 +0200

    Working on the grammar edition

---
+++ src/main/java/com/gramevapp/web/controller/GrammarController.java
@@ -0,0 +1,46 @@
+package com.gramevapp.web.controller;
+
+import com.gramevapp.web.model.Experiment;
+import com.gramevapp.web.model.Grammar;
+import com.gramevapp.web.model.User;
+import com.gramevapp.web.repository.GrammarRepository;
+import com.gramevapp.web.service.ExperimentService;
+import com.gramevapp.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class GrammarController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @Autowired
+    private GrammarRepository grammarRepository;
+
+
+    @RequestMapping(value="/user/grammarRepository", method= RequestMethod.GET)
+    public String experimentRepository(Model model){
+
+        User user = userService.getLoggedInUser();
+        if(user == null){
+            System.out.println("User not authenticated");
+            return "redirect:/login";
+        }
+
+        List<Grammar> grammarList = grammarRepository.findAll();
+
+        model.addAttribute("grammarList", grammarList);
+        model.addAttribute("user", user);
+
+        return "user/grammarRepository";
+    }
+
+
+}
