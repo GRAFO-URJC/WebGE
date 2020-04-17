bd975f3872076b3904352eb067e350746e6e4011
lei <l.chen1.2016@alumnos.urjc.es>
2020-04-17 12:34:58 +0200

    #27 - fixed problem with grammar delete button

--- src/main/java/com/gramevapp/web/controller/GrammarController.java
+++ src/main/java/com/gramevapp/web/controller/GrammarController.java
@@ -7,10 +7,12 @@ import com.gramevapp.web.repository.GrammarRepository;
 import com.gramevapp.web.service.ExperimentService;
 import com.gramevapp.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -28,11 +30,11 @@ public class GrammarController {
     private GrammarRepository grammarRepository;
 
 
-    @RequestMapping(value="/grammar/grammarRepository", method= RequestMethod.GET)
-    public String grammarRepository(Model model){
+    @RequestMapping(value = "/grammar/grammarRepository", method = RequestMethod.GET)
+    public String grammarRepository(Model model) {
 
         User user = userService.getLoggedInUser();
-        if(user == null){
+        if (user == null) {
             System.out.println("User not authenticated");
             return "redirect:/login";
         }
@@ -46,23 +48,30 @@ public class GrammarController {
     }
 
 
-    @RequestMapping(value="/grammar/grammarRepoSelected", method=RequestMethod.POST, params="deleteGrammar")
+    @RequestMapping(value = "/grammar/grammarRepoSelected", method = RequestMethod.POST, params = "deleteGrammar")
     public
     @ResponseBody
-    Long expRepoSelectedDelete(@RequestParam("grammarId") String grammarId){
+    Long expRepoSelectedDelete(@RequestParam("grammarId") String grammarId) {
         Long idGrammar = Long.parseLong(grammarId);
 
-        grammarRepository.deleteById(idGrammar);
+        try {
+            grammarRepository.deleteById(idGrammar);
+        } catch (DataIntegrityViolationException e) {
+            System.out.println(e.getCause() instanceof org.hibernate.exception.ConstraintViolationException);
+            if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+                return Long.valueOf(-1);
+            }
+        }
 
         return idGrammar;
     }
 
 
-    @RequestMapping(value="/grammar/grammarDetail", method= RequestMethod.POST)
-    public String editGrammar(Model model, @RequestParam("grammarId") String grammarId){
+    @RequestMapping(value = "/grammar/grammarDetail", method = RequestMethod.POST)
+    public String editGrammar(Model model, @RequestParam("grammarId") String grammarId) {
 
         User user = userService.getLoggedInUser();
-        if(user == null){
+        if (user == null) {
             System.out.println("User not authenticated");
             return "redirect:/login";
         }
@@ -80,8 +89,8 @@ public class GrammarController {
         return "grammar/grammarDetail";
     }
 
-    @RequestMapping(value="/grammar/saveGrammar", method=RequestMethod.POST)
-    public String saveGrammar(Model model, @ModelAttribute("grammar") @Valid Grammar gr){
+    @RequestMapping(value = "/grammar/saveGrammar", method = RequestMethod.POST)
+    public String saveGrammar(Model model, @ModelAttribute("grammar") @Valid Grammar gr) {
         gr.setCreationDate(new Date(System.currentTimeMillis()));
         grammarRepository.save(gr);
         return grammarRepository(model);
