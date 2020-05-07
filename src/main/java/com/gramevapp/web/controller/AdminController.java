4549ea2cbe9cb02931c4c14338b8d37edba79762
lei <l.chen1.2016@alumnos.urjc.es>
2020-05-07 23:09:42 +0200

    #45 - admin page empty added, configure new redirect after login and deleted register button

---
+++ src/main/java/com/gramevapp/web/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.gramevapp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin/adminPage";
+    }
+}
