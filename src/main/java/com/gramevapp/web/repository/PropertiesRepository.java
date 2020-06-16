49efa077bf169b1799d747bd31e4651250dfdadb
Lei Chen Xu <lei.chen.xu@outlook.es>
2020-06-16 12:43:11 +0200

    #80 - remove experiment properties table and use

--- src/main/java/com/gramevapp/web/repository/PropertiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gramevapp.web.repository;
-
-import com.gramevapp.web.model.ExpProperties;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertiesRepository extends JpaRepository<ExpProperties, Long> {
-}
