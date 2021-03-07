4421919dc27580bee7abca6197c840edc121fb33
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2021-03-08 00:17:07 +0100

    Correct logger.

--- src/main/java/com/engine/algorithm/SymbolicRegressionGE.java
+++ src/main/java/com/engine/algorithm/SymbolicRegressionGE.java
@@ -104,10 +104,10 @@ public class SymbolicRegressionGE extends AbstractProblemGE {
     public void stopExecution() {
         stop = true;
         algorithm.stopExection();
-        Logger.getLogger(this.getClass().getName()).log(Level.INFO,">>>> GE STOPPED - "+Thread.currentThread().getName());
+        logger.log(Level.INFO,">>>> GE STOPPED - "+Thread.currentThread().getName());
         if (this.DE) {
             alg.stopExection();
-            Logger.getLogger(this.getClass().getName()).log(Level.INFO,">>>> DE STOPPED - "+Thread.currentThread().getName());
+            logger.log(Level.INFO,">>>> DE STOPPED - "+Thread.currentThread().getName());
 
         }
     }
